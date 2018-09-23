06f9d2d8223cd7aee0aa59f893f486b931693b5d
Nathan Hering <nhering@hotmail.com>
2018-09-23 16:46:35 -0700

    Add project files.

---
+++ Funtility/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Funtility.Startup))]
+namespace Funtility
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
