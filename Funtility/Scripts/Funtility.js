133554ca01dcec5845292b2972d9dbe76e15099a
Nathan Hering <nhering@hotmail.com>
2018-10-21 14:14:38 -0700

    Fixed game-area height issue

--- Funtility/Scripts/Funtility.js
+++ Funtility/Scripts/Funtility.js
@@ -1,15 +1,22 @@
-﻿// Accepts an array of elementIDs and a CssClass
-function ToggleCssClass(elementIDs, CssClass) {
+﻿var debugging = true;
+
+// elementIDs: A single string or an array of strings. The name of one or multiple elements by ID.
+// cssClass: A string the name of the class to add to the elements class list.
+function ToggleCssClass(elementIDs, cssClass) {
+    if (debugging) console.log("ToggleCssClass()");
+
     if (typeof elementIDs === 'string') {
-        document.getElementById(elementIDs).classList.toggle(CssClass);
+        document.getElementById(elementIDs).classList.toggle(cssClass);
     } else {
         for (i = 0; i < elementIDs.length; i++) {
-            document.getElementById(elementIDs[i]).classList.toggle(CssClass);
+            document.getElementById(elementIDs[i]).classList.toggle(cssClass);
         }
     }
 }
 
 window.onclick = function (event) {
+    if (debugging) console.log("window.onclick()");
+
     if (!event.target.matches('.dropdown-button')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         for (let i = 0; i < dropdowns.length; i++) {
@@ -22,11 +29,19 @@ window.onclick = function (event) {
 }
 
 function ResizeGameAreaHeight() {
-    let newHeight = GetElementDimensions('site-main').height;
+    if (debugging) console.log("ResizeGameAreaHeight()");
+
+    let newHeight = window.innerHeight
+    let siteHeader = GetElementDimensions('site-header').height;
+    let gameHeader = GetElementDimensions('game-header').height;
+    let siteFooter = GetElementDimensions('site-footer').height;
+    newHeight -= (siteHeader + gameHeader + siteFooter);
     document.getElementById('game-area').style.height = newHeight + "px";
 }
 
 function GetElementDimensions(ElementId) {
+    if (debugging) console.log("GetElementDimensions()");
+
     let width = document.getElementById(ElementId).clientWidth;
     let height = document.getElementById(ElementId).clientHeight;
     return {
